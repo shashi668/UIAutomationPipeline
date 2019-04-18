1d14ca4f9c7dbf4e6352011609ce51c6fc8e2615
auto_user1 <shashank.raju668@gmail.com>
2019-04-18 22:59:11 +0530

    Git Commit UI Automation

---
+++ src/main/java/WebAutomation/GUIAutomation/Browserlaunch.java
@@ -0,0 +1,13 @@
+package WebAutomation.GUIAutomation;
+
+import org.openqa.selenium.WebDriver;
+
+public class Browserlaunch {
+
+	public static void main(String[] args)
+	{
+		System.err.println("Run from maven");
+		System.out.println("===========================>>");
+		System.out.println("<<**************************>>");
+	}
+}
---
+++ src/test/java/automation/test/Firstprogrammingtest.java
@@ -0,0 +1,11 @@
+package automation.test;
+
+public class Firstprogrammingtest {
+
+	public static void main(String[] args) {
+		 
+		System.out.println("My first program");
+		System.out.println("Integrate with Git Hub");
+	}
+
+}
